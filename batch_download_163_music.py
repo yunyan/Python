ca2b8dc73a9ac0b6631f2dd5c87c0bbb59e9eee3
yunyan.li <kobe.net@gmail.com>
2017-01-08 00:52:09 -0800

    add option parser

--- batch_download_163_music.py
+++ batch_download_163_music.py
@@ -2,21 +2,40 @@
 
 import sys
 import re
+import getopt
 from subprocess import call
 from urllib.request import urlopen
 
-def main(url):
-    add = url[0]
-    add = add.replace("#/", "")
+def main(url, dest):
+    url = url.replace("#/", "")
     download_list = []
-    rs = urlopen(add).read().decode('utf8')
+    rs = urlopen(url).read().decode('utf8')
     l = re.findall(r'\/song\?id=\d+', rs)
     for i in l:
         download_list.append("http://music.163.com/#"+i)
     for i in download_list:
-        call(["you-get", i])
+        call(["you-get", "-o", dest, i])
         
 
 
 if __name__ == '__main__':
-    main(sys.argv[1:])
+    try:
+        opt, args = getopt.getopt(sys.argv[1:], "s:o:")
+    except getopt.GetoptError as err:
+        print (err)
+        
+    if len(opt) == 0:
+        print("Usage:")
+        print(sys.argv[0], "-s [url] -o [destination]")
+        sys.exit(1)
+
+    url = None
+    dest = "."
+    for o, v in opt:
+        if o == "-s":
+            url = v
+        elif o == "-o":
+            dest = v
+    main(url, dest)
+
+            
