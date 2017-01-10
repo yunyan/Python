52e50ce8be3ec6833052c3e29956b4ea2afd1c05
Yunyan <yunyann.li@gmail.com>
2017-01-10 09:40:39 +0800

    updated

--- hash_cal.py
+++ hash_cal.py
@@ -8,18 +8,18 @@ def main(argv):
     path = argv[0]
     md5_dict = {}
     for dirpath, dirname, filelist in os.walk(path):
-        print dirpath, dirname, filelist
+        print (dirpath, dirname, filelist)
         for infile in filelist:
             file_with_path = os.path.join(dirpath, infile)
-            with open(file_with_path, 'r') as openfile:
+            with open(file_with_path, 'r', encoding='utf-8') as openfile:
                 m = hashlib.md5()
-                m.update(openfile.read())
+                m.update(openfile.read().encode('utf-8'))
                 md5_value = m.hexdigest()
                 if md5_value not in md5_dict:
                     md5_dict[md5_value] = file_with_path
-                    print "adding %s:%s to md5_dict..." % (md5_value, file_with_path)
+                    print ("adding {0}:{1} to md5_dict...".format(md5_value, file_with_path))
                 else:
-                    print "deleting %s ..." % file_with_path
+                    print ("deleting {0} ...".format(file_with_path))
 
 if __name__ == '__main__':
     main(sys.argv[1:])
