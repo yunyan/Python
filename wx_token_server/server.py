6f1f9c378b8ee68467c21a8faeca65e5c18142c4
yunyan <kobe.net@gmail.com>
2017-02-27 17:08:26 +0800

    Add token server for wechat

---
+++ wx_token_server/server.py
@@ -0,0 +1,71 @@
+import socket
+import select
+import sys
+import token_thread
+import os
+import signal
+
+token_dict = {}
+close_socks_flag = False
+
+def sigint_handler(signum, frame):
+    print "you press ctrl-c"
+    close_socks_flag =  True
+
+def main():
+    HOST = socket.gethostname()
+    PORT = 50007
+    potential_readers = []
+    potential_writers = []
+    potential_errors = []
+    threads = []
+    server_sock = None
+
+#    signal.signal(signal.SIGINT, sigint_handler)
+
+    for res in socket.getaddrinfo(HOST, PORT, socket.AF_UNSPEC, socket.SOCK_STREAM, 
+            0, socket.AI_PASSIVE):
+        af, socktype, proto, cannoname, sa = res
+        print sa
+        try:
+            server_sock =  socket.socket(af, socktype, proto)
+            server_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        except socket.error as msg:
+            server_sock =  None
+            continue
+        try:
+            server_sock.bind(sa)
+            server_sock.listen(5)
+            potential_readers.append(server_sock)
+        except socket.error as msg:
+            server_sock.close()
+            server_sock =  None
+            continue
+        break
+    if server_sock is None:
+        print "Could not open socket"
+        sys.exit(1)
+
+    while True:
+        print "waiting for connection\n"
+        ready_to_read, ready_to_write, in_error = \
+                select.select(
+                        potential_readers,
+                        potential_writers,
+                        potential_errors,
+                        5)
+
+        for s in ready_to_read:
+            print s, "is ready to read"
+            conn, addr = s.accept()
+            t = token_thread.token_thread([conn, addr])
+            t.start()
+            threads.append(t)
+
+        for t in threads:
+            t.join()
+
+
+
+if __name__ == '__main__':
+    main()
---
+++ wx_token_server/token_thread.py
@@ -0,0 +1,62 @@
+import threading
+import urllib2
+import ssl
+import datetime
+import json
+
+tokens  =  {}
+
+class token_thread(threading.Thread):
+    _conn = None 
+    _addr = None 
+    _operation = None
+    _app_id = None
+    _app_sec = None 
+    _lock = threading.Lock()
+
+    def __init__(self, net_info):
+        threading.Thread.__init__(self)
+        [self._conn, self._addr] = net_info 
+
+    
+    def refresh_token(self):
+        request_token_cmd = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid={APPID}&secret={APPSECRET}".format(APPID=self._app_id, APPSECRET=self._app_sec)
+        context = ssl._create_unverified_context()
+        response = urllib2.urlopen(request_token_cmd, context = context).read()
+        result = json.loads(response)
+        access_token = result[u'access_token']
+        expires_in = result[u'expires_in']
+        expires_time = datetime.datetime.now() + datetime.timedelta(seconds=expires_in)
+        self._lock.acquire()
+        tokens[self._app_id] = [access_token, expires_time]
+        self._lock.release()
+        return tokens[self._app_id][0]
+
+
+    def apply_token(self):
+        if not tokens.has_key(self._app_id):
+            self.refresh_token()
+
+        [token, expires_time] =  tokens[self._app_id]
+        now = datetime.datetime.now()
+        time_rest = now - expires_time 
+        if abs(time_rest.total_seconds()) < 10:
+            print "rest seconds < 10", time_rest.total_seconds()
+            self.refresh_token()
+
+        return tokens[self._app_id][0]
+
+    def run(self):
+        data = self._conn.recv(1024)
+        [self._operation, self._app_id, self._app_sec] = data.split('|')
+        try:
+            if self._operation == "APPLY":
+                token = self.apply_token()
+            elif self._operation == "REFRESH":
+                token = self.refresh_token()
+            print token
+            self._conn.send(token)
+        except Exception as err:
+            print err
+        finally:
+            self._conn.close()
