80346e1ec0f70b9aeb57129a545b2944a17dc259
Yunyan <yunyann.li@gmail.com>
2018-01-30 21:56:31 +0800

    aaa

--- APKParser/ap_info_parser.py
+++ APKParser/ap_info_parser.py
@@ -1,10 +1,14 @@
 import ap_logging
 
 logger = ap_logging.logging.getLogger(__name__)
+
 class ap_info_parser(object):
     apk_info = []
     def __init__(self):
         pass
+
+    def __call__(self):
+        self.parse()
 
     def get_apk_info(self):
         return self.apk_info
