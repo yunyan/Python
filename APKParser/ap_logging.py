74b299aa857000343fbf196d5861ec0aea8503da
Yunyan <yunyann.li@gmail.com>
2018-01-30 22:27:25 +0800

    aaa

--- APKParser/ap_logging.py
+++ APKParser/ap_logging.py
@@ -1,5 +1,4 @@
 import logging
 
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-logging.getLogger('requests').setLevel(logging.CRITICAL)
 logger = logging.getLogger("APKParser")
